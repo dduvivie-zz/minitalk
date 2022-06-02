166833f15cd2880e6cc9683c51a61d2d87bf73d8
Danae Duvivier <dduvivie@shi-r7-p1.s19.be>
2022-06-02 19:37:51 +0200

    header file

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 17:43:53 by dduvivie          #+#    #+#             */
+/*   Updated: 2022/06/02 17:43:54 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <signal.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+# include "./libft/ft_printf/ft_printf.h"
+
+int	test1(void);
+int test2(void);
+
+# endif
